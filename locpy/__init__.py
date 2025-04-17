0ee1aed935b9d4f12150ee768fa8877a8f1e1129
Pigpen42305 <pigpen42305@hotmail.com>
2025-04-17 11:47:26 -0700

    Changed the exports to just EntryData and corrected error logging

--- locpy/__init__.py
+++ locpy/__init__.py
@@ -5,10 +5,14 @@ FIRST_DIR = os.getcwd()
 try: 
     from LoC_API import EntryData
 except Exception as Error:
-    print(repr(Error,file = open(os.path.dirname(__file__),'w')))
+    print(repr(Error,file = open(os.path.join(os.path.dirname(__file__),'loc_error.txt'),'w')))
+    exit("AN ERROR OCCURRED")
+
 
 if os.getcwd() != FIRST_DIR:
     os.chdir(FIRST_DIR)
 del os,FIRST_DIR
 
-
+__all__ = [
+    'EntryData',
+]
