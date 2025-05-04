c8aaeb0910507cce69e9aae37d4221e6a04afa4e
Pigpen42305 <pigpen42305@hotmail.com>
2025-05-03 17:03:51 -0700

    Added an example script that will gather up the transcripts into a folder.

---
+++ Example_Transcript_Getter.py
@@ -0,0 +1,46 @@
+"""
+Will get all of the transcripts that actually work.
+The transcripts will be saved in a folder called 'TRANSCRIPTS'
+The items must provide resources, and a transcript in those resources with a fulltext url.
+"""
+from os import chdir,mkdir
+
+try:
+    chdir('TRANSCRIPTS')
+except FileNotFoundError:
+    mkdir('TRANSCRIPTS')
+    chdir('TRANSCRIPTS')
+
+from locpy import EntryData
+
+
+skipped = []
+for title,item in EntryData.title_instances.items():
+    try:
+        item.get_transcript(title + '.txt',timeout=5)
+    except NotImplementedError:
+        pass
+    except KeyError:
+        print(f'\r<--{item.index} RAISES AN ERROR!!!-->')
+    except TimeoutError:
+        skipped.append((title,item))
+        print(f'\r<--{item.index} SKIPPED FOR NOW-->')
+
+I = 0
+while len(skipped) > 0:
+    current = skipped.copy()
+    skipped = []
+    for title,item in current:
+        try:
+            item.get_transcript(title + '.txt',timeout=5)
+        except NotImplementedError:
+            pass
+        except TimeoutError:
+            skipped.append((title,item))
+            print(f'\r<--{item.index} SKIPPED FOR NOW-->')
+    if current == skipped:
+        I += 1
+    else:
+        I = 0
+    if I >= 10:
+        raise TimeoutError(f"Retries to get these items have made no progress!: {[i.index for _,i in skipped]}")
